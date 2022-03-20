825b0de5cb428037173defc1f90791d7a435b9ab
zReko <52071362+zReko@users.noreply.github.com>
2022-03-20 10:06:52 +0200

    hook loading

---
+++ hooks/Main.lua
@@ -0,0 +1,33 @@
+local hook_files = {
+    ["lib/setups/gamesetup"] =                                      {"gamesetup.lua"},
+    ["lib/setups/setup"] =                                          {"setup.lua"},
+    ["core/lib/setups/coresetup"] =                                 {"coresetup.lua"},
+}
+if not zMenuTools then
+    local function getScriptPath()
+        local string = debug.getinfo(2, "S").source:sub(2)
+        return string:match("(.*/)")
+    end
+    local function stringExplode(string, separator)
+        local parts = {}
+        for part in string:gmatch("([^" .. separator .. "]+)") do
+            table.insert(parts, part)
+        end
+        return parts
+    end
+    local scriptPathTable = stringExplode(getScriptPath(), "/")
+    local modPath = scriptPathTable[1] .. "/" .. scriptPathTable[2] .. "/"
+    dofile(modPath .. "Tools/SDK.lua")
+    zMenuTools:assign_path(modPath)
+end
+
+if RequiredScript then
+	local requiredScript_key = RequiredScript:lower()
+    if hook_files[requiredScript_key] then
+        for _, file in ipairs(hook_files[requiredScript_key]) do
+            if SystemFS:exists(zMenuTools:modPath() .. "hooks/" .. file) then
+                dofile(zMenuTools:modPath() .. "hooks/" .. file)
+            end
+        end
+	end
+end
--- tools/Tools.lua
+++ tools/Tools.lua
@@ -1,3 +1,14 @@
 local zMenuToolsClass = class()
-
-zMenuToolsClass = zMenuToolsClass:new()
+function zMenuToolsClass:has_mod_path()
+    if self.mod_path and self.mod_path ~= "" and string.len(self.mod_path) > 3 then
+        return true
+    end
+    return false
+end
+function zMenuToolsClass:assign_path(path)
+    self.mod_path = path
+end
+function zMenuToolsClass:mod_path()
+    return self.mod_path or ""
+end
+zMenuTools = zMenuToolsClass:new()
