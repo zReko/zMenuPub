6a792cfbc375949065c46fd900fdcb6505de1b12
zReko <52071362+zReko@users.noreply.github.com>
2022-03-20 18:05:41 +0200

    clamp resizing

--- menu/items/menu_transform.lua
+++ menu/items/menu_transform.lua
@@ -5,11 +5,25 @@ end
 function zMenuClass:updateMenuSize(x,y)
     local new_x = self.last_update_x and (x - self.last_update_x) or 0 
     local new_y = self.last_update_y and (y - self.last_update_y) or 0 
+    local should_update_x = true
+    local should_update_y = true
+    if (self.menu_master_panel:w() <= 300) and new_x < 0 then
+        should_update_x = false
+        new_x = 0
+    end
+    if self.menu_master_panel:h() <= 300 and new_y < 0 then
+        should_update_y = false
+        new_y = 0
+    end
     self.menu_master_panel:grow(new_x,new_y)
     self.feature_panel:grow(new_x,new_y)
     self.left_side_panel:grow(0,new_y)
     self.profile_pic:set_y(self.menu_master_panel:h()-95)
     self.master_panel_mouse_resize_panel:set_position(self.menu_master_panel:w()-18,self.menu_master_panel:h()-18)
-    self.last_update_x = x
-    self.last_update_y = y
+    if should_update_x then
+        self.last_update_x = x
+    end
+    if should_update_y then
+        self.last_update_y = y
+    end
 end
